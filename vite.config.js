3c7088c98fb2101584b5d25d840450427492c402
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-24 14:31:25 +0100

    changed file path

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     css: true,
-    setupFiles: './src/assets/setupTests.js',
+    setupFiles: './src/setupTests.js',
 },
 })
