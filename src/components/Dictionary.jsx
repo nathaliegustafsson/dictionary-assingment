4358929a3156415c40670275721d76ab402bf28f
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-27 14:12:58 +0100

    possible to use search

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -1,34 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 function DictionaryExample() {
     const [wordData, setWordData] = useState(null);
     const [error, setError] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/sky');
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-
-                const data = await response.json();
-                setWordData(data);
-            } catch (error) {
-                setError(error.message);
+    const fetchData = async (searchTerm) => {
+        try {
+            const response = await fetch(
+                `https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`
+            );
+            if (!response.ok) {
+                throw new Error(`Word not found`);
             }
-        };
 
-        fetchData();
-    }, []);
+            const data = await response.json();
+            setWordData(data);
+        } catch (error) {
+            setError(error.message);
+        }
+    };
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    if (!wordData) {
-        return <div>Loading...</div>;
-    }
+    const handleSearch = () => {
+        if (searchTerm.trim() !== '') {
+            fetchData(searchTerm);
+        }
+    };
 
     return (
         <div
@@ -49,51 +46,87 @@ function DictionaryExample() {
                     marginBottom: '2rem',
                 }}
             >
-                <input
-                    type="text"
-                    placeholder="Search..."
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault(); // Prevent the default form submission behavior
+                        handleSearch(); // Call your search function
+                    }}
                     style={{
                         backgroundColor: '#F4F3F2',
-                        fontFamily: 'Roboto Mono, monospace',
+                        display: 'flex',
+                        justifyContent: 'space-between',
                         padding: '0.5rem',
                         border: 'none',
-                        borderRadius: '10px',
+                        borderRadius: '7px',
                         boxShadow: '0px 3px 10.300000190734863px rgba(0, 0, 0, 0.25)',
                         width: '450px',
                         height: '20px',
                     }}
-                />
+                >
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        style={{
+                            backgroundColor: '#F4F3F2',
+                            fontFamily: 'Roboto Mono, monospace',
+                            border: 'none',
+                            width: '100%',
+                            outline: 'none',
+                        }}
+                    />
+                    <button
+                        type="submit"
+                        onClick={handleSearch}
+                        style={{
+                            border: 'none',
+                            backgroundColor: 'transparent',
+                            padding: 0,
+                            marginBottom: '1rem',
+                            cursor: 'pointer',
+                        }}
+                    >
+                        <span className="material-symbols-outlined" style={{ fontSize: '22px' }}>
+                            search
+                        </span>
+                    </button>
+                </form>
             </div>
-            <h1>{wordData[0].word}</h1>
 
-            <h2>Phonetics</h2>
-            <ul>
-                {wordData[0].phonetics.map((phonetic, index) => (
-                    <li key={index}>
-                        <p>{phonetic.text}</p>
-                        {phonetic.audio && (
-                            <audio controls>
-                                <source src={phonetic.audio} type="audio/mp3" />
-                            </audio>
-                        )}
-                    </li>
-                ))}
-            </ul>
+            {wordData && wordData.length > 0 && (
+                <div>
+                    <h1>{wordData[0].word}</h1>
 
-            <h2>Meanings</h2>
-            {wordData[0].meanings.map((meaning, index) => (
-                <div key={index}>
-                    <p>{meaning.partOfSpeech}</p>
+                    <h2>Phonetics</h2>
                     <ul>
-                        {meaning.definitions.map((definition, index) => (
+                        {wordData[0].phonetics.map((phonetic, index) => (
                             <li key={index}>
-                                <p>{definition.definition}</p>
-                                {/* You can add more details here, such as synonyms, antonyms, and examples */}
+                                <p>{phonetic.text}</p>
+                                {phonetic.audio && (
+                                    <audio controls>
+                                        <source src={phonetic.audio} type="audio/mp3" />
+                                    </audio>
+                                )}
                             </li>
                         ))}
                     </ul>
+
+                    <h2>Meanings</h2>
+                    {wordData[0].meanings.map((meaning, index) => (
+                        <div key={index}>
+                            <p>{meaning.partOfSpeech}</p>
+                            <ul>
+                                {meaning.definitions.map((definition, index) => (
+                                    <li key={index}>
+                                        <p>{definition.definition}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
-            ))}
+            )}
         </div>
     );
 }
