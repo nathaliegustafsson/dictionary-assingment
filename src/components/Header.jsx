8f6f99d15897d44adc5c678567c6de71c062a0be
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-27 10:52:21 +0100

    add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,22 @@ function Header() {
     return (
         <div
             className="header-container"
-            style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}
+            style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '2rem',
+                marginBottom: '2rem',
+            }}
         >
-            <h1 style={{ fontFamily: 'Playfair Display, serif' }}>Dictionary</h1>
+            <h1
+                style={{
+                    fontFamily: 'Playfair Display, serif',
+                    fontSize: '2.5rem',
+                    fontWeight: '300',
+                }}
+            >
+                Dictionary
+            </h1>
         </div>
     );
 }
