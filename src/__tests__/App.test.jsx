f78494b60142a42089da868747c7c5a4a86e88ad
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-27 16:10:35 +0100

    add two more tests

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,7 +1,35 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 test('if there is a header with correct title', () => {
     render(<App />);
     expect(screen.getByText('Dictionary')).toBeInTheDocument();
+});
+
+test('should display searched word via click', async () => {
+    render(<App />);
+    const user = userEvent.setup();
+
+    const searchInput = screen.getByRole('textbox');
+    await user.type(searchInput, 'world');
+
+    const button = screen.getByRole('button');
+    await user.click(button, { name: /search/i });
+
+    await waitFor(() => {
+        expect(screen.getByText('world')).toBeInTheDocument();
+    });
+});
+
+test('should display searched word via enter', async () => {
+    render(<App />);
+    const user = userEvent.setup();
+
+    const searchInput = screen.getByRole('textbox');
+    await user.type(searchInput, 'world{Enter}');
+
+    await waitFor(() => {
+        expect(screen.getByText('world')).toBeInTheDocument();
+    });
 });
