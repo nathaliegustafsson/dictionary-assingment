419ba35b32b014b96202e4cf6fb20b1bbc1b925e
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-27 10:54:54 +0100

    first test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,7 @@
+import { render, screen } from '@testing-library/react';
 import App from './App';
-import { render, screen } from '@testing-library/react'
 
-test('if there is a hello world', () => {
+test('if there is a header with correct title', () => {
     render(<App />);
-    const text = screen.getByText('Hello World');
-    expect(text).toBeInTheDocument();
+    expect(screen.getByText('Dictionary')).toBeInTheDocument();
 });
