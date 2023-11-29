84658d0ab7f82939e07e650a37637636c1f40cf3
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-29 22:41:27 +0100

    changed name on component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import DictionaryExample from './components/Dictionary';
+import Dictionary from './components/Dictionary';
 import Header from './components/Header';
 
 function App() {
     return (
         <div>
             <Header />
-            <DictionaryExample />
+            <Dictionary />
         </div>
     );
 }
