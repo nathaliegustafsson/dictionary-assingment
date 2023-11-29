7f0a5ba3c0ea0811a4726b64e7cf49ac9c3eb3d0
Nathalie Gustafsson <nath.gustafsson@gmail.com>
2023-11-29 23:10:12 +0100

    add test for text

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -2,75 +2,170 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 
-test('if there is a header with correct title', () => {
-    render(<App />);
-    expect(screen.getByText('Dictionary')).toBeInTheDocument();
-});
-
-test('should display searched word via click', async () => {
-    render(<App />);
-    const user = userEvent.setup();
-
-    const searchInput = screen.getByRole('textbox');
-    await user.type(searchInput, 'world');
-
-    const button = screen.getByRole('button');
-    await user.click(button, { name: /search/i });
-
-    await waitFor(() => {
-        expect(screen.getByText('world', { selector: 'h2' })).toBeInTheDocument();
+describe('Title visability', () => {
+    test('if there is a header with correct title', () => {
+        render(<App />);
+        expect(screen.getByText('Dictionary')).toBeInTheDocument();
     });
 });
 
-test('should display searched word via enter', async () => {
-    render(<App />);
-    const user = userEvent.setup();
+describe('Search Functionality', () => {
+    test('should display searched word via click', async () => {
+        render(<App />);
+        const user = userEvent.setup();
 
-    const searchInput = screen.getByRole('textbox');
-    await user.type(searchInput, 'world{Enter}');
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'world');
 
-    await waitFor(() => {
-        expect(screen.getByText('world')).toBeInTheDocument();
+        const button = screen.getByRole('button');
+        await user.click(button, { name: /search/i });
+
+        await waitFor(() => {
+            expect(screen.getByText('world', { selector: 'h2' })).toBeInTheDocument();
+        });
+    });
+
+    test('should display searched word via enter', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'world{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('world')).toBeInTheDocument();
+        });
     });
 });
 
-test('should display error message when searching for a word that does not exist ', async () => {
-    render(<App />);
-    const user = userEvent.setup();
+describe('Search Functionality', () => {
+    test('should display error message when searching for a word that does not exist ', async () => {
+        render(<App />);
+        const user = userEvent.setup();
 
-    const searchInput = screen.getByRole('textbox');
-    await user.type(searchInput, 'Katt{Enter}');
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'Katt{Enter}');
 
-    await waitFor(() => {
-        expect(screen.getByText('Word not found. Please try again.')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText('Word not found. Please try again.')).toBeInTheDocument();
+        });
+    });
+
+    test('should display error message when the search is empty', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, '{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('Please enter a word to search.')).toBeInTheDocument();
+        });
     });
 });
 
-test('should display error message when the search is empty', async () => {
-    render(<App />);
-    const user = userEvent.setup();
+describe('Audio Functionality', () => {
+    test('should render the audio element', async () => {
+        render(<App />);
+        const user = userEvent.setup();
 
-    const searchInput = screen.getByRole('textbox');
-    await user.type(searchInput, '{Enter}');
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'coffee{Enter}');
 
-    await waitFor(() => {
-        expect(screen.getByText('Please enter a word to search.')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText('coffee')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const audioElement = screen.getByAltText('Play Icon');
+            expect(audioElement).toBeInTheDocument();
+        });
     });
 });
 
-test('should render the audio element', async () => {
-    render(<App />);
-    const user = userEvent.setup();
+describe('Meaning Display', () => {
+    test('should render phonetics text', async () => {
+        render(<App />);
+        const user = userEvent.setup();
 
-    const searchInput = screen.getByRole('textbox');
-    await user.type(searchInput, 'coffee{Enter}');
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'coffee{Enter}');
 
-    await waitFor(() => {
-        expect(screen.getByText('coffee')).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText('coffee')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const phoneticText = screen.getByText('/ˈkɒ.fi/');
+            expect(phoneticText).toBeInTheDocument();
+        });
     });
 
-    await waitFor(() => {
-        const audioElement = screen.getByAltText('Play Icon');
-        expect(audioElement).toBeInTheDocument();
+    test('should render partOfSpeech text', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'coffee{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('coffee')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const partOfSpeech = screen.getByText('noun');
+            expect(partOfSpeech).toBeInTheDocument();
+        });
+    });
+
+    test('should render meaning text', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'coffee{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('coffee')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const meaningText = screen.getByText('To drink coffee.');
+            expect(meaningText).toBeInTheDocument();
+        });
+    });
+
+    test('should render an synonym text', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'hello{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('hello')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const meaningText = screen.getByText('greeting');
+            expect(meaningText).toBeInTheDocument();
+        });
+    });
+
+    test('should render an antonym text', async () => {
+        render(<App />);
+        const user = userEvent.setup();
+
+        const searchInput = screen.getByRole('textbox');
+        await user.type(searchInput, 'up{Enter}');
+
+        await waitFor(() => {
+            expect(screen.getByText('up')).toBeInTheDocument();
+        });
+
+        await waitFor(() => {
+            const meaningText = screen.getByText('down');
+            expect(meaningText).toBeInTheDocument();
+        });
     });
 });
